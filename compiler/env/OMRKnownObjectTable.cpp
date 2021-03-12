3d5d1bf1f3d773cea016a5011cfaa44fbdeeb79e
Liqun Liu <liqunl@ca.ibm.com>
2021-03-12 04:27:30 +0000

    Add definition to OMR::KnownObjectTable::UNKNOWN to fix the odr problem
    Signed-off-by: Liqun Liu <liqunl@ca.ibm.com>

--- compiler/env/OMRKnownObjectTable.cpp
+++ compiler/env/OMRKnownObjectTable.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2020 IBM Corp. and others
+ * Copyright (c) 2000, 2021 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -27,6 +27,7 @@
 #include "compile/Compilation.hpp"
 #include "env/CompilerEnv.hpp"
 
+const OMR::KnownObjectTable::Index OMR::KnownObjectTable::UNKNOWN = -1;
 
 OMR::KnownObjectTable::KnownObjectTable(TR::Compilation *comp) :
       _comp(comp),
--- compiler/env/OMRKnownObjectTable.hpp
+++ compiler/env/OMRKnownObjectTable.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2020 IBM Corp. and others
+ * Copyright (c) 2000, 2021 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -66,7 +66,7 @@ public:
    TR::KnownObjectTable * self();
 
    typedef int32_t Index;
-   static const Index UNKNOWN = -1;
+   static const Index UNKNOWN;
 
    TR_FrontEnd *fe() const { return _fe; }
    void setFe(TR_FrontEnd *fe) { _fe = fe; }
